9c7257d439e157d9e0b8a261b1d9694e870787cf
zziro95 <opers7878@gmail.com>
2020-11-05 19:11:45 +0900

    codeUp 1001,1002,1003,1004번 문제풀이

---
+++ codeUp_1001_1002_1003_1004.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+//codeUp 1001 : 출력하기 01
+//Hello를 출력하는 문제
+print("Hello")
+
+//codeUp 1002 : 출력하기 02
+//Hello World를 출력하는 문제
+print("Hello World")
+
+//codeUp 1003 : 출력하기 03
+//줄을 바꿔 Hello World 출력하기
+//print를 두번나눠 써도 되지만 한print문에 줄바꿈키워드인 \n을 써도 된다
+print("Hello")
+print("World")
+
+print("Hello\nWorld")
+
+//codeUp 1004 : 출력하기 04
+//'Hello' 출력하기
+// 작은 따옴표 문자 ' 를 출력하기 위해서는 \' 로 입력해야 한다.
+print("\'Hello\'")
+
